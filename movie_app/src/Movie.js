12ec5eae6ac748c4281770ae9e6659561ad412fb
siri_06 <siri_06@rnd.skinfosec.com>
2018-12-11 14:39:58 +0900

    react 강의내용 1차 커밋

---
+++ movie_app/src/Movie.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import './Movie.css';
+
+class Movie extends Component {
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <MoivePoster poster={this.props.poster} title={this.props.title} />
+      </div>
+    )
+  }
+}
+
+class MoivePoster extends Component {
+  //propTypes : 부모 컴포넌트에서 받은 정보 체크 
+  //isRequired : 해당 정보는 필수로 받아야한다 라고 표시함, 내용이 없을 경우 console에서 에러 메세지 표시
+  static propTypes = {
+    poster: PropTypes.string.isRequired
+  }
+  render() {
+    return (
+      <img src={this.props.poster} alt={this.props.title} />
+    )
+  }
+}
+
+
+export default Movie;
